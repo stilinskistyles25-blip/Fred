8811b5831c9f1b2fefd7799a232f336c17ca93e4
stilinskistyles25-blip <stilinskistyles25@gmail.com>
2025-10-30 19:03:12 +0300

    Add files via upload

---
+++ Weekly_revenue_tracker.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ int main {
+ 
+   float revenue[7], total revenue=0.0, average revenue;
+      int i;
+  
+    for (i=0; i<7; i++){
+    printf("Enter revenue for day %d: i+1");
+    scanf("%f" ,&revenue [i]);
+    total += revenue[i] 
+        }
+        average revenue= total / 7;
+        printf("Total weekly revenue =%.2nf \n, total");
+        printf("Average daily revenue=%.2f \n, average");
+        
+        return 0;
+
+}
